6872cbc2fe877e1acc3e844c3da6ac80610ef6d4
kwdowik <wdowikkacper@gmail.com>
2020-03-30 16:21:54 +0200

    Moved book tests' cases to json file

--- src/model/book.test.js
+++ src/model/book.test.js
@@ -1,43 +1,8 @@
 const Book = require('./book');
+const testsCases = require('./book.json');
 
 describe('book.js', () => {
-    [
-        {
-            testName: 'should be valid',
-            book: {
-                id: '1',
-                author: 'author name',
-                title: 'title'
-            },
-            expected: true,
-        },
-        {
-            testName: 'should be invalid - id is required',
-            book: {
-                author: 'author name',
-                title: 'title'
-            },
-            expected: false,
-        },
-        {
-            testName: 'should be invalid - author name must be longer than 3 characters',
-            book: {
-                id: '1',
-                author: 'aut',
-                title: 'title'
-            },
-            expected: false,
-        },
-        {
-            testName: 'should be invalid - title must be longer than 1 character',
-            book: {
-                id: '1',
-                author: 'author name',
-                title: 't'
-            },
-            expected: false,
-        }
-    ].forEach(({ testName, book, expected }) =>
+    testsCases.forEach(({ testName, book, expected }) =>
         it(testName, () => {
             expect(new Book({...book}).isValid()).toBe(expected);
         })
